27ceb70301ad770d345d570828c836264f277304
Luis Bosquez <luisdbosquez@gmail.com>
2017-12-03 23:58:40 -0800

    Adding python sample

--- README.md
+++ README.md
@@ -1,57 +1,47 @@
-# Project Name
+---
+services: cosmos-db
+platforms: python
+author: luisbosquez
+---
 
-(short, 1-3 sentenced, description of the project)
+# Developing a Python Gremlin app using Azure Cosmos DB
+Azure Cosmos DB is a globally distributed multi-model database. One of the supported APIs is the Graph (Gremlin) API, which provides a graph data model with [Gremlin query/traversals](https://tinkerpop.apache.org/gremlin.html). This sample shows you how to use the Azure Cosmos DB with the Graph API to store and access data from a Python application.
 
-## Features
+## Running this sample
 
-This project framework provides the following features:
+* Before you can run this sample, you must have the following prerequisites:
 
-* Feature 1
-* Feature 2
-* ...
+   * An active Azure account. If you don't have one, you can sign up for a [free account](https://azure.microsoft.com/free/). Alternatively, you can use the [Azure Cosmos DB Emulator](https://azure.microsoft.com/documentation/articles/documentdb-nosql-local-emulator) for this tutorial.
+   * JDK 1.7+ (Run `apt-get install default-jdk` if you don't have JDK)
+   * Maven (Run `apt-get install maven` if you don't have Maven)
 
-## Getting Started
+* Then, clone this repository using `git clone https://github.com/Azure-Samples/azure-cosmos-db-graph-python-getting-started.git`
 
-### Prerequisites
+* Next, substitute the endpoint and authorization key in the `connect.py`, on line 105, with your Cosmos DB account's values:
 
-(ideally very short, if any)
+```python
+client = client.Client('wss://<YOUR_ENDPOINT>:443/','g', 
+        username="/dbs/<YOUR_DATABASE>/colls/<YOUR_COLLECTION_OR_GRAPH>", 
+        password="<YOUR_PASSWORD>")
+```
 
-- OS
-- Library version
-- ...
+| Setting | Suggested Value | Description |
+| ------- | --------------- | ----------- |
+| YOUR_ENDPOINT   | [***.graphs.azure.com] | This is the Gremlin URI value on the Overview page of the Azure portal, in square brackets, with the trailing :443/ removed.  This value can also be retrieved from the Keys tab, using the URI value by removing https://, changing documents to graphs, and removing the trailing :443/. |
+| port | 443 | Set the port to 443 |
+| username | `/dbs/<db>/colls/<coll>` | The resource of the form `/dbs/<db>/colls/<coll>` where `<db>` is your database name and `<coll>` is your collection name. |
+| password | Your primary key | This is your primary key, which you can retrieve from the Keys page of the Azure portal, in the Primary Key box. Use the copy button on the left side of the box to copy the value. |
 
-### Installation
+* From a command prompt or shell, run `pip install -r requirements.txt` to get and resolve dependencies.
 
-(ideally very short)
+* From a command prompt or shell, run `python connect.py` to run the application.
 
-- npm install [package name]
-- mvn install
-- ...
+## About the code
+The code included in this sample is intended to get you quickly started with a Python application that connects to Azure Cosmos DB with the Graph (Gremlin) API.
 
-### Quickstart
-(Add steps to get up and running quickly)
+## More information
 
-1. git clone [repository clone url]
-2. cd [respository name]
-3. ...
-
-
-## Demo
-
-A demo app is included to show how to use the project.
-
-To run the demo, follow these steps:
-
-(Add steps to start up the demo)
-
-1.
-2.
-3.
-
-## Resources
-
-(Any additional resources or related projects)
-
-- Link to supporting information
-- Link to similar sample
-- ...
+- [Azure Cosmos DB](https://docs.microsoft.com/azure/cosmos-db/introduction)
+- [Azure Cosmos DB : Graph API](https://docs.microsoft.com/en-us/azure/cosmos-db/graph-introduction)
+- [Gremlin Python](http://tinkerpop.apache.org/docs/current/reference/#gremlin-python)
+- [Gremlin-Python driver source code](https://github.com/apache/tinkerpop/tree/master/gremlin-python)
---
+++ connect.py
@@ -0,0 +1,148 @@
+from gremlin_python.driver import client
+import sys, traceback
+
+_gremlin_cleanup_graph = "g.V().drop()"
+
+_gremlin_insert_vertices = [
+    "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)",
+    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)",
+    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller')",
+    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield')"
+]
+
+_gremlin_insert_edges = [
+    "g.V('thomas').addE('knows').to(g.V('mary'))",
+    "g.V('thomas').addE('knows').to(g.V('ben'))",
+    "g.V('ben').addE('knows').to(g.V('robin'))"   
+]
+
+_gremlin_update_vertices = [
+    "g.V('thomas').property('age', 44)"
+]
+
+_gremlin_count_vertices = "g.V().count()"
+
+_gremlin_traversals = {
+    "Get all persons older than 40"          : "g.V().hasLabel('person').has('age', gt(40)).values('firstName', 'age')",
+    "Get all persons and their first name"   : "g.V().hasLabel('person').values('firstName')",
+    "Get all persons sorted by first name"   : "g.V().hasLabel('person').order().by('firstName', incr).values('firstName')",
+    "Get all persons that Thomas knows"      : "g.V('thomas').out('knows').hasLabel('person').values('firstName')",
+    "People known by those who Thomas knows" : "g.V('thomas').out('knows').hasLabel('person').out('knows').hasLabel('person').values('firstName')",
+    "Get the path from Thomas to Robin"      : "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()"
+}
+
+_gremlin_drop_operations = {
+    "Drop Edge - Thomas no longer knows Mary" : "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()",
+    "Drop Vertex - Drop Thomas"               : "g.V('thomas').drop()"
+}
+
+def cleanup_graph(client):
+    print("\tRunning this Gremlin query:\n\t{0}".format(_gremlin_cleanup_graph))
+    callback = client.submitAsync(_gremlin_cleanup_graph)
+    if callback.result() is not None:
+        print("\tCleaned up the graph!")
+    print("\n")
+
+def insert_vertices(client):
+    for query in _gremlin_insert_vertices:
+        print("\tRunning this Gremlin query:\n\t{0}\n".format(query))
+        callback = client.submitAsync(query)
+        if callback.result() is not None:
+            print("\tInserted this vertex:\n\t{0}\n".format(callback.result().one()))
+        else:
+            print("Something went wrong with this query: {0}".format(query))
+    print("\n")
+
+def insert_edges(client):
+    for query in _gremlin_insert_edges:
+        print("\tRunning this Gremlin query:\n\t{0}\n".format(query))
+        callback = client.submitAsync(query)
+        if callback.result() is not None:
+            print("\tInserted this edge:\n\t{0}\n".format(callback.result().one()))
+        else:
+            print("Something went wrong with this query:\n\t{0}".format(query))
+    print("\n")
+
+def update_vertices(client):
+    for query in _gremlin_update_vertices:
+        print("\tRunning this Gremlin query:\n\t{0}\n".format(query))
+        callback = client.submitAsync(query)
+        if callback.result() is not None:
+            print("\tUpdated this vertex:\n\t{0}\n".format(callback.result().one()))
+        else:
+            print("Something went wrong with this query:\n\t{0}".format(query))
+    print("\n")
+
+def count_vertices(client):
+    print("\tRunning this Gremlin query:\n\t{0}".format(_gremlin_count_vertices))
+    callback = client.submitAsync(_gremlin_count_vertices)
+    if callback.result() is not None:
+        print("\tCount of vertices: {0}".format(callback.result().one()))
+    else:
+        print("Something went wrong with this query: {0}".format(_gremlin_count_vertices))
+    print("\n")
+
+def execute_traversals(client):
+    for key in _gremlin_traversals:
+        print("\t{0}:".format(key))
+        print("\tRunning this Gremlin query:\n\t{0}\n".format(_gremlin_traversals[key]))
+        callback = client.submitAsync(_gremlin_traversals[key])
+        for result in callback.result():
+            print("\t{0}".format(str(result)))
+        print("\n")
+
+def execute_drop_operations(client):
+    for key in _gremlin_drop_operations:
+        print("\t{0}:".format(key))
+        print("\tRunning this Gremlin query:\n\t{0}".format(_gremlin_drop_operations[key]))
+        callback = client.submitAsync(_gremlin_drop_operations[key])
+        for result in callback.result():
+            print(result)
+        print("\n")
+
+try:
+    client = client.Client('wss://<YOUR_ENDPOINT>:443/','g', 
+        username="/dbs/<YOUR_DATABASE>/colls/<YOUR_COLLECTION_OR_GRAPH>", 
+        password="<YOUR_PASSWORD>")
+    
+    print("Welcome to Azure Cosmos DB + Gremlin on Python!")
+    
+    # Drop the entire Graph
+    raw_input("We're about to drop whatever graph is on the server. Press any key to continue...")
+    cleanup_graph(client)
+
+    # Insert all vertices
+    raw_input("Let's insert some vertices into the graph. Press any key to continue...")
+    insert_vertices(client)
+
+    # Create edges between vertices
+    raw_input("Now, let's add some edges between the vertices. Press any key to continue...")
+    insert_edges(client)
+    
+
+    # Update a couple of vertices
+    raw_input("Ah, sorry. I made a mistake. Let's change the ages of these two vertices. Press any key to continue...")
+    update_vertices(client)
+
+    # Count all vertices
+    raw_input("Okay. Let's count how many vertices we have. Press any key to continue...")
+    count_vertices(client)
+
+    # Execute traversals and get results 
+    raw_input("Cool! Let's run some traversals on our graph. Press any key to continue...")
+    execute_traversals(client)
+
+    # Drop a few vertices and edges
+    raw_input("So, life happens and now we will make some changes to the graph. Press any key to continue...")
+    execute_drop_operations(client)
+
+    # Count all vertices again
+    raw_input("How many vertices do we have left? Press any key to continue...")
+    count_vertices(client)
+
+except Exception as e:
+    print('There was an exception: {0}'.format(e))
+    traceback.print_exc(file=sys.stdout)
+
+print("\nAnd that's all! Sample complete")
+raw_input("Press Enter to continue...")
