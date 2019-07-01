41264971e864f88e30b93fa7bccede05ad76a52f
Stephen Tramer <sttramer@microsoft.com>
2019-07-01 11:19:00 -0700

    Updated Python content to comply with PEP8 (via autopep8)

--- connect.py
+++ connect.py
@@ -1,5 +1,6 @@
 from gremlin_python.driver import client, serializer
-import sys, traceback
+import sys
+import traceback
 
 _gremlin_cleanup_graph = "g.V().drop()"
 
@@ -13,7 +14,7 @@ _gremlin_insert_vertices = [
 _gremlin_insert_edges = [
     "g.V('thomas').addE('knows').to(g.V('mary'))",
     "g.V('thomas').addE('knows').to(g.V('ben'))",
-    "g.V('ben').addE('knows').to(g.V('robin'))"   
+    "g.V('ben').addE('knows').to(g.V('robin'))"
 ]
 
 _gremlin_update_vertices = [
@@ -23,92 +24,108 @@ _gremlin_update_vertices = [
 _gremlin_count_vertices = "g.V().count()"
 
 _gremlin_traversals = {
-    "Get all persons older than 40"          : "g.V().hasLabel('person').has('age', gt(40)).values('firstName', 'age')",
-    "Get all persons and their first name"   : "g.V().hasLabel('person').values('firstName')",
-    "Get all persons sorted by first name"   : "g.V().hasLabel('person').order().by('firstName', incr).values('firstName')",
-    "Get all persons that Thomas knows"      : "g.V('thomas').out('knows').hasLabel('person').values('firstName')",
-    "People known by those who Thomas knows" : "g.V('thomas').out('knows').hasLabel('person').out('knows').hasLabel('person').values('firstName')",
-    "Get the path from Thomas to Robin"      : "g.V('thomas').repeat(out()).until(has('id', 'robin')).path().by('firstName')"
+    "Get all persons older than 40": "g.V().hasLabel('person').has('age', gt(40)).values('firstName', 'age')",
+    "Get all persons and their first name": "g.V().hasLabel('person').values('firstName')",
+    "Get all persons sorted by first name": "g.V().hasLabel('person').order().by('firstName', incr).values('firstName')",
+    "Get all persons that Thomas knows": "g.V('thomas').out('knows').hasLabel('person').values('firstName')",
+    "People known by those who Thomas knows": "g.V('thomas').out('knows').hasLabel('person').out('knows').hasLabel('person').values('firstName')",
+    "Get the path from Thomas to Robin": "g.V('thomas').repeat(out()).until(has('id', 'robin')).path().by('firstName')"
 }
 
 _gremlin_drop_operations = {
-    "Drop Edge - Thomas no longer knows Mary" : "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()",
-    "Drop Vertex - Drop Thomas"               : "g.V('thomas').drop()"
+    "Drop Edge - Thomas no longer knows Mary": "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()",
+    "Drop Vertex - Drop Thomas": "g.V('thomas').drop()"
 }
 
+
 def cleanup_graph(client):
-    print("\tRunning this Gremlin query:\n\t{0}".format(_gremlin_cleanup_graph))
+    print("\tRunning this Gremlin query:\n\t{0}".format(
+        _gremlin_cleanup_graph))
     callback = client.submitAsync(_gremlin_cleanup_graph)
     if callback.result() is not None:
         print("\tCleaned up the graph!")
     print("\n")
+
 
 def insert_vertices(client):
     for query in _gremlin_insert_vertices:
         print("\tRunning this Gremlin query:\n\t{0}\n".format(query))
         callback = client.submitAsync(query)
         if callback.result() is not None:
-            print("\tInserted this vertex:\n\t{0}\n".format(callback.result().one()))
+            print("\tInserted this vertex:\n\t{0}\n".format(
+                callback.result().one()))
         else:
             print("Something went wrong with this query: {0}".format(query))
     print("\n")
+
 
 def insert_edges(client):
     for query in _gremlin_insert_edges:
         print("\tRunning this Gremlin query:\n\t{0}\n".format(query))
         callback = client.submitAsync(query)
         if callback.result() is not None:
-            print("\tInserted this edge:\n\t{0}\n".format(callback.result().one()))
+            print("\tInserted this edge:\n\t{0}\n".format(
+                callback.result().one()))
         else:
             print("Something went wrong with this query:\n\t{0}".format(query))
     print("\n")
+
 
 def update_vertices(client):
     for query in _gremlin_update_vertices:
         print("\tRunning this Gremlin query:\n\t{0}\n".format(query))
         callback = client.submitAsync(query)
         if callback.result() is not None:
-            print("\tUpdated this vertex:\n\t{0}\n".format(callback.result().one()))
+            print("\tUpdated this vertex:\n\t{0}\n".format(
+                callback.result().one()))
         else:
             print("Something went wrong with this query:\n\t{0}".format(query))
     print("\n")
 
+
 def count_vertices(client):
-    print("\tRunning this Gremlin query:\n\t{0}".format(_gremlin_count_vertices))
+    print("\tRunning this Gremlin query:\n\t{0}".format(
+        _gremlin_count_vertices))
     callback = client.submitAsync(_gremlin_count_vertices)
     if callback.result() is not None:
         print("\tCount of vertices: {0}".format(callback.result().one()))
     else:
-        print("Something went wrong with this query: {0}".format(_gremlin_count_vertices))
+        print("Something went wrong with this query: {0}".format(
+            _gremlin_count_vertices))
     print("\n")
+
 
 def execute_traversals(client):
     for key in _gremlin_traversals:
         print("\t{0}:".format(key))
-        print("\tRunning this Gremlin query:\n\t{0}\n".format(_gremlin_traversals[key]))
+        print("\tRunning this Gremlin query:\n\t{0}\n".format(
+            _gremlin_traversals[key]))
         callback = client.submitAsync(_gremlin_traversals[key])
         for result in callback.result():
             print("\t{0}".format(str(result)))
         print("\n")
 
+
 def execute_drop_operations(client):
     for key in _gremlin_drop_operations:
         print("\t{0}:".format(key))
-        print("\tRunning this Gremlin query:\n\t{0}".format(_gremlin_drop_operations[key]))
+        print("\tRunning this Gremlin query:\n\t{0}".format(
+            _gremlin_drop_operations[key]))
         callback = client.submitAsync(_gremlin_drop_operations[key])
         for result in callback.result():
             print(result)
         print("\n")
 
+
 try:
-    client = client.Client('wss://<YOUR_ENDPOINT>.gremlin.cosmosdb.azure.com:443/','g', 
-        username="/dbs/<YOUR_DATABASE>/colls/<YOUR_COLLECTION_OR_GRAPH>", 
-        password="<YOUR_PASSWORD>",
-        message_serializer=serializer.GraphSONSerializersV2d0()
-    )
-    
+    client = client.Client('wss://<YOUR_ENDPOINT>.gremlin.cosmosdb.azure.com:443/', 'g',
+                           username="/dbs/<YOUR_DATABASE>/colls/<YOUR_COLLECTION_OR_GRAPH>",
+                           password="<YOUR_PASSWORD>",
+                           message_serializer=serializer.GraphSONSerializersV2d0()
+                           )
+
     print("Welcome to Azure Cosmos DB + Gremlin on Python!")
-    
+
     # Drop the entire Graph
     input("We're about to drop whatever graph is on the server. Press any key to continue...")
     cleanup_graph(client)
@@ -120,7 +137,6 @@ try:
     # Create edges between vertices
     input("Now, let's add some edges between the vertices. Press any key to continue...")
     insert_edges(client)
-    
 
     # Update a couple of vertices
     input("Ah, sorry. I made a mistake. Let's change the ages of these two vertices. Press any key to continue...")
@@ -130,7 +146,7 @@ try:
     input("Okay. Let's count how many vertices we have. Press any key to continue...")
     count_vertices(client)
 
-    # Execute traversals and get results 
+    # Execute traversals and get results
     input("Cool! Let's run some traversals on our graph. Press any key to continue...")
     execute_traversals(client)
 
